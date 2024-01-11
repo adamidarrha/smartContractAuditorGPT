88ae5ce111b4d67a3eb60a15ba30bb9b6cfef8e7
adamIdarrha <adamidarrha@gmail.com>
2024-01-11 07:55:13 +0100

    d

---
+++ api/wsgi.py
@@ -0,0 +1,4 @@
+from soloditWrapperAPI import app
+
+if __name__ == "__main__":
+    app.run()
--- gunicorn/wsgi.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, './api/')
-
-import soloditWrapperAPI
-
-if __name__ == "__main__":
-    soloditWrapperAPI.app.run()
